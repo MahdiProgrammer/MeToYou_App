c3f7cfa11f42362066a114e3a13553e5702aa60a
Mahdi Noori <94842704+MahdiProgrammer@users.noreply.github.com>
2022-05-14 18:56:35 -0700

    Add files via upload

---
+++ image_request.php
@@ -0,0 +1,22 @@
+<?php
+if($_SERVER['REQUEST_METHOD']== 'POST')
+{
+	$image = $_POST['image'];
+	$name = $_POST['name'];
+	
+	$id = rand(100,1000);
+	$imageName = "img"."_".$name.$id;
+	
+	$path = "upload/$imageName.png";
+	
+	file_put_contents($path,base64_decode($image));
+	
+	
+	
+	echo "https://mahdiprogrammer.github.io/MeToYou_App/$path";
+}else{
+	echo "Error";
+	
+}
+	
+?>
