a72e534b01a516585d40c9223525f60a009ef7c0
Mahdi Noori <94842704+MahdiProgrammer@users.noreply.github.com>
2022-05-16 07:34:29 +0430

    Update image_request.php

--- image_request.php
+++ image_request.php
@@ -13,7 +13,7 @@ if($_SERVER['REQUEST_METHOD']== 'POST')
 	
 	
 	
-	echo "https://github.com/MahdiProgrammer/MeToYou_App/tree/main/$path";
+	echo "https://github.com/MahdiProgrammer/MeToYou_App/$path";
 }else{
 	echo "Error";
 	
